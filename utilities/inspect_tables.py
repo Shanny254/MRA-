a6fa187be92d1766700dd1430e79d68a90f7f3b7
Sharlyne Nyaboke <shanny254-sharlynenyaboke@gmail.com>
2026-07-13 12:14:58 +0300

    Add utility script for inspecting Excel table schemas

---
+++ utilities/inspect_tables.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from scripts.extract import get_sheet_names
+from scripts.extract import read_sheet
+
+for sheet in get_sheet_names():
+
+    print("=" * 70)
+    print(sheet.upper())
+
+    df = read_sheet(sheet)
+
+    print(df.columns.tolist())
