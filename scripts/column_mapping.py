0385bc56af80afab8cdb5882b48926106b7ae566
Sharlyne Nyaboke <shanny254-sharlynenyaboke@gmail.com>
2026-07-14 14:58:28 +0300

    updated colum_mapping

--- scripts/column_mapping.py
+++ scripts/column_mapping.py
@@ -1,22 +1,27 @@
 COLUMN_MAPPING = {
 
+    # Account Information
     "Account Number": "account_number",
     "ACCOUNT": "account_number",
     "New Account No.": "account_number",
     "Old Account No.": "old_account_number",
 
+    # Customer Information
+    "Customer": "customer_name",
     "Customer Name": "customer_name",
     "Customers Name": "customer_name",
-    "Customer": "customer_name",
     "Cliente": "customer_name",
 
+    # Staff Information
     "Staff": "staff_name",
     "Staff No": "staff_no",
 
+    # Meter Information
     "Meters": "meter_number",
     "Meter": "meter_number",
     "Meter No.": "meter_number",
 
+    # Location
     "Region": "region",
     "REGION": "region",
 
@@ -30,47 +35,54 @@ COLUMN_MAPPING = {
     "Zone Name": "zone_name",
     "ZONE_NAME": "zone_name",
 
+    # Routing
     "Itin": "itinerary",
     "Itin ": "itinerary",
     "Itinerary": "itinerary",
 
+    # Business Information
+    "BU": "business_unit",
+    "Bu": "business_unit",
+
+    "Office Name": "office_name",
+
+    "Reading Unit": "reading_unit",
+
+    "Supply Location": "supply_location",
+
     "Contract Status": "contract_status",
 
+    # Tariff
     "Tariff": "tariff",
     "Cod Tariff": "tariff",
     "COD_TARIFF": "tariff",
 
+    # Communication
     "COMM STATUS": "communication_status",
 
+    # Coordinates
     "GOOGLE_LAT": "latitude",
     "GOOGLE_LONG": "longitude",
 
-    "BU": "business_unit",
-    "Bu": "business_unit",
-
-    "Reading Unit": "reading_unit",
-
-    "Office Name": "office_name",
-
-    "Supply Location": "supply_location",
-
+    # Billing
     "Period": "period",
-
     "Bill Amount": "bill_amount",
+    "Amount Kes": "amount_kes",
+    "Units Kwhrs": "units_kwhrs",
 
+    # Meter Reading
     "Reader Code": "reader_code",
-
     "Meter Reader": "meter_reader",
-
-    "Reading Frm Mr": "reading_frm_mr",
-
+    "Reader Name": "meter_reader",
+    "Reading Frm Mr": "reading_from_meter",
     "Prev Value": "previous_value",
 
+    # Usage
     "Usage Type": "usage_type",
 
+    # Estimation
     "Ind Estimate": "estimated_reading",
-
+    "Ind Estimated": "estimated_reading",
     "Ind Self Read": "self_read",
-
     "Times Billed Zero": "times_billed_zero"
 }
